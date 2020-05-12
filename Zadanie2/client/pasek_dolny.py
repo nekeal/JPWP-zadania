67418bc0739cb363b2718ba5c1b4082977fc789d
Szymon Cader <szymon.SC.cader@gmail.com>
2020-05-12 11:06:36 +0200

    ZADANIE 2.2

--- Zadanie2/client/pasek_dolny.py
+++ Zadanie2/client/pasek_dolny.py
@@ -17,11 +17,10 @@ class PasekDolny:
         self.czysc_button = PrzyciskTekstowy(810, 490 ,110 ,55, (128,128,128), "Czyść")
         self.gumka_button = PrzyciskTekstowy(925,490,110, 55, (128,128,128), "Gumka")
         self.flaga = True
-
-
+        self.color = (0, 0, 0)
 
     def rysuj(self, win):
-        pygame.draw.rect(win, (0,0,0), (self.x, self.y, self.SZEROKOSC, self.WYSOKOSC), self.OBRAMOWANIE)
+        pygame.draw.rect(win, self.color, (self.x, self.y, self.SZEROKOSC, self.WYSOKOSC), self.OBRAMOWANIE)
         self.czysc_button.rysuj(win)
         self.gumka_button.rysuj(win)
 
@@ -35,6 +34,9 @@ class PasekDolny:
             self.gra.tablica.czyszczenie()
             self.gra.connection.send({10:[]})
 
-        #if self.gumka_button.klikniecie(*mouse):
+        if self.gumka_button.klikniecie(*mouse):
           ### Napisz kod do zadania nr 2
-
+            if self.color == (0, 0, 0):
+                self.color = (255, 255, 255)
+            else:
+                self.color = (0, 0, 0)
